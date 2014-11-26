2ca7cc27304c770e6453158037cdf382e51b60a8
Michael <wanshibo1992@163.com>
2014-11-26 09:45:47 +0800

    make kernel

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 490 */
+/* This file is auto generated, version 491 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#490 SMP PREEMPT Tue Nov 25 16:02:01 CST 2014"
+#define UTS_VERSION "#491 SMP PREEMPT Wed Nov 26 09:42:08 CST 2014"
 #define LINUX_COMPILE_BY "mic"
 #define LINUX_COMPILE_HOST "micpc"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
